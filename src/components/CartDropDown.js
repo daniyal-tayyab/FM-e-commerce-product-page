9cfeaf948913d6eecb58f9c929bca8465b07efd8
testuserdani <testemaildani96@gmail.com>
2023-02-24 18:23:03 +0500

    final changes in dropdown

--- src/components/CartDropDown.js
+++ src/components/CartDropDown.js
@@ -2,12 +2,20 @@ import React from "react";
 import Button from "./Button";
 import CartItem from "./CartItem";
 
+const items = [1];
+
 const CartDropDown = () => {
   return (
-    <div className="cart-dropdown">
-      <div>Cart</div>
-      <CartItem />
-      <Button>Checkout</Button>
+    <div className={items.length ? "cart-dropdown" : "cart-dropdown-empty"}>
+      <div className={items.length ? "" : "cart-text"}>Cart</div>
+      {items.length ? (
+        <>
+          <CartItem />
+          <Button>Checkout</Button>
+        </>
+      ) : (
+        <p>Your cart is empty</p>
+      )}
     </div>
   );
 };
